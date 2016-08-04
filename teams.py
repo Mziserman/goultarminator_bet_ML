a73ee815c5066eba88c48818d92045270060a92d
Martin Ziserman <martinziserman@gmail.com>
2016-08-04 02:44:16 +0200

    cool

--- teams.py
+++ teams.py
@@ -6,42 +6,59 @@ from scrapy.http.request import Request
 import json
 import re
 
-
-
 class OfficialSpider(CrawlSpider):
     name = 'official'
     start_urls = []
     done = []
+    servers = {}
 
     def start_requests(self):
         with open('games.json') as data_file:
-        	games = json.load(data_file)
-        	for game in games:
-        		yield Request(game['winner']['url'], self.parse)
-        		yield Request(game['loser']['url'], self.parse)
+            games = json.load(data_file)
+            for game in games:
+                yield Request(game['winner']['url'], self.parse)
+                yield Request(game['loser']['url'], self.parse)
 
     def parse(self, response):
-    	players = []
-    	team = {}
-    	team['players'] = players
+        players = []
+        team = {}
+        team['players'] = players
 
-    	team['id'] = response.url.split('/')[-1][:+4]
+        team['id'] = response.url.split('/')[-1][:+4]
 
-    	if team['id'] in done :
-    		yield
-    	else:
-    		done.append(team['id'])
+        if team['id'] in self.done:
+            yield
+        else:
+            self.done.append(team['id'])
 
-    	team['name'] = re.sub('-', ' ', response.url.split('/')[-1][+5:])
+        team['name'] = re.sub('-', ' ', response.url.split('/')[-1][+5:])
+        team['server'] = team['name'][:-2]
 
-    	for div in response.css('.ak-panel-content .ak-list-element .ak-content'):
-    		name = div.css('.ak-title a strong::text').extract_first()
-    		classe = div.css('.ak-text::text').extract_first()
-    		classe = classe[:-11]
+        composition = []
 
-    		player = {}
-    		player['name'] = name
-    		player['classe'] = classe
-    		team['players'].append(player)
-    		
-    	yield team
+        for div in response.css('.ak-panel-content .ak-list-element .ak-content'):
+            name = div.css('.ak-title a strong::text').extract_first()
+            classe = div.css('.ak-text::text').extract_first()
+            classe = classe[:-11]
+
+            if classe not in composition:
+                composition.append(classe)
+
+            player = {}
+            player['name'] = name
+            player['classe'] = classe
+            team['players'].append(player)
+
+        team['composition'] = composition
+
+        if team['server'] not in self.servers:
+            server = {}
+            server['name'] = team['server']
+            server['teams'] = [team]
+            self.servers[server['name']] = server
+        else:
+            self.servers[team['server']]['teams'].append(team)
+            if len(self.servers[team['server']]['teams']) == 4:
+                yield self.servers[team['server']]
+            else:
+            	print(team['server'])
